9bd531f0ad4fda4c398737f24b13d554168a1cd8
SenanS <30498489+SenanS@users.noreply.github.com>
2019-03-30 20:14:05 +0000

    Added note for modified argument parser

--- udp/router_send.cpp
+++ udp/router_send.cpp
@@ -23,15 +23,23 @@ main(int argc, char *argv[])
 	char ipstr[INET6_ADDRSTRLEN], buf[BUFFER_SIZE];
 	std::string router_port = DEFAULT_PORT;
 
+	
+	
+	//Modified argument parser goes here
+	
+	
 	// Set command argument as router port if entered
-	switch(argc){ 
-		case 1: break;
-		case 2: router_port = argv[1]; break;
-		default:		
-			fprintf(stderr, "usage: ./router [port]\n"); 
-			exit(0); 
-	}
+	//switch(argc){ 
+	//	case 1: break;
+	//	case 2: router_port = argv[1]; break;
+	//	default:		
+	//		fprintf(stderr, "usage: ./router [port]\n"); 
+	//		exit(0); 
+	//}
 
+	
+	
+	
 	// Setup up UDP socket at given port
 	router_fd = setup_socket_udp(router_port.c_str());	
 	
